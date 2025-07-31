16e1fe6215ea322bd183372f4823b193e334a8ae
sindhujagathees <155611392+sindhujagathees@users.noreply.github.com>
2025-07-31 13:52:56 +0530

    Add files via upload

--- FaceBookRegistration.java
+++ FaceBookRegistration.java
@@ -7,30 +7,55 @@ import org.openqa.selenium.support.ui.Select;
 public class FaceBookRegistration {
 
 	public static void main(String[] args) {
+		
+		//To launch in a guest mode
 		ChromeOptions options = new ChromeOptions();
+		
+		// Launch the browser
 		ChromeDriver driver1 = new ChromeDriver(options);
+		
+		// Load the url - get
 		driver1.get("https://en-gb.facebook.com/");
+		
+		// Maximize the browser
 		driver1.manage().window().maximize();
-		//driver1.findElement(By.id("username")).sendKeys("DemoCSR");
-
-		//driver1.findElement(By.id("password")).sendKeys("crmsfa");
+		
+		// Click on create new account
 		driver1.findElement(By.linkText("Create new account")).click();
-		driver1.findElement(By.id("createnewaccount_firstName")).sendKeys("Sindhuja");
-
 		
-		driver1.findElement(By.id("createnewaccount_lastName")).sendKeys("Jagatheesan");
-		WebElement sourceElement = driver1.findElement(By.id("createnewaccount_dataSourceId"));
-		Select dropdown = new Select(sourceElement);
-		dropdown.selectByValue("Day");
-		dropdown.selectByValue("Month");
-		dropdown.selectByValue("Year");
-		WebElement sourceElement1 = driver1.findElement(By.id("//input[@value='male']"));
-		Select radio1=new Select(sourceElement1);
-		radio1.selectByValue("male");
-		radio1.selectByValue("female");
-		radio1.selectByValue("custom");
+		// Enter the firstname
+		driver1.findElement(By.name("firstname")).sendKeys("Sindhuja");
 		
-        driver1.findElement(By.name("SignUP")).click();
+		// Enter the lastname
+		driver1.findElement(By.name("lastname")).sendKeys("Jagatheesan");
+		
+		// Find the dropdown element
+		WebElement sourceElement = driver1.findElement(By.id("day"));
+		
+		// Step2: Select-Create an Object
+		Select click=new Select(sourceElement);
+		
+		// select by index
+		click.selectByIndex(1);
+		WebElement sourceElement1 = driver1.findElement(By.id("month"));
+		Select click1=new Select(sourceElement1);
+		click1.selectByIndex(2);
+		WebElement sourceElement2 = driver1.findElement(By.id("year"));
+		Select click2=new Select(sourceElement2);
+		click2.selectByValue("1981");
+		
+		// Enter the email address
+		driver1.findElement(By.name("reg_email__")).sendKeys("sinja2011@gmail.com");
+		
+		// Enter the password
+		driver1.findElement(By.name("reg_passwd__")).sendKeys("******");
+		
+		// Find the element by gender and value
+		driver1.findElement(By.xpath("//input[@name='sex' and @value='1']")).click();
+		
+		// Click on the signup button
+        driver1.findElement(By.name("websubmit")).click();
+       // driver1.close();
 	}
 
 }
